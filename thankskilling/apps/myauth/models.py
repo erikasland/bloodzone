1d5ee98c060d95b8b063a4a193f3de1d5e81767a
mehrik <sobreo.m@gmail.com>
2015-11-24 15:34:59 -0800

    Created users db_table within myauth models.py

--- thankskilling/apps/myauth/models.py
+++ thankskilling/apps/myauth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=254)
+    pw_hash = models.CharField(max_length=255)
+    created_at = models.DateField()
+    class Meta:
+        db_table = 'users'
