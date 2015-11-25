c132c1306aa9edceaac5dd45a7c4864bc3ea566c
Erik Asland <erikasland87@gmail.com>
2015-11-25 09:58:35 -0800

    nahmees work

--- thankskilling/thankskilling/urls.py
+++ thankskilling/thankskilling/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', include('apps.myauth.urls')),
+    url(r'^', include('apps.myauth.urls')),
     url(r'^movies', include('apps.movies.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
